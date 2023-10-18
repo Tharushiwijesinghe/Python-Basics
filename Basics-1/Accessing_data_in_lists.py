d888264de3ba6a7de052142dddce8f26df334e65
Tharushiwijesinghe <134124219+Tharushiwijesinghe@users.noreply.github.com>
2023-10-19 00:44:38 +0530

    Create Accessing_data_in_lists.py

---
+++ Basics-1/Accessing_data_in_lists.py
@@ -0,0 +1,9 @@
+simple_list = [1,5,2,7,3,66,1923,11]
+#we can access multiple values in the list using the :
+#we must be careful though
+print(simple_list[2:5])
+#why is not 66 in the output
+#the number to the left of the : is inclusive, but the number to the right is exclusive
+print(simple_list[2:3])
+#we can also use negative numbers
+print(simple_list[-5:-1])
