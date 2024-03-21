72773fc615bd1af74ba20c9d1a3c8c400d3d817d
Tharushiwijesinghe <134124219+Tharushiwijesinghe@users.noreply.github.com>
2024-03-21 09:09:32 +0530

    Create Functions.py

---
+++ Basics-1/Functions.py
@@ -0,0 +1,16 @@
+def functionName(arg1):
+    print(arg1)
+
+functionName("Hello")
+
+
+
+
+def func(length, width):
+    perimeter = 2 * (length + width)
+    area = length * width
+    print("Perimeter:", perimeter)
+    print("Area:", area)
+
+func(4, 8)
+func(2, 1)
