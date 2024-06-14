b7367888fd6dd8ebbb679e4c75cfd50dcf858464
Tharushiwijesinghe <134124219+Tharushiwijesinghe@users.noreply.github.com>
2024-06-14 23:05:04 +0530

    Create answers.py

---
+++ Exercises/Strings in Python/answers.py
@@ -0,0 +1,11 @@
+#q1
+street = "River street"
+city = "Ambalantota"
+country = "Sri Lanka"
+
+print(street + ",\n" + city + ",\n" + country + ".")
+
+print("\n")
+
+address = f'{street},\n{city},\n{country}.'
+print(address)
